2d337eebf95d19d358756be9653a702ef8a43c02
Leandro Faustino <leandrofaustiinoo@hotmail.com>
2024-01-10 20:31:52 -0300

    atualizando1

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-$(document).ready(function(){
-    $('form button').click(function(){
-        $('ul li').slideDown();
-    })
-    $('ul li').click(function(){
-        $('ul li').slideUp();
-    })
-    $('form').on('submit', function(e){
-        console.log("submit");
-        e.preventDefault();
+const form = document.getElementById('form-agenda');
+const nome = [];
+let linhas = '';
 
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha(){
+    const inputNomeContato = document.getElementById('nome-contato')
+
+
+        nome.push(inputNomeContato.value);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+    }
+
+    inputNomeContato.value = '';
+
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    corpoTabela.addEventListener('click', function (e){
+        if(e.target.classList.contains('checked')){
+            e.target.classList.remove('checked');
+        }else{
+            e.target.classList.add('checked');
+        }
     })
-})
+
+}
