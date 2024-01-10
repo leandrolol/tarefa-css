1437671848eec097c8931aa89dfd8894e8f1cb6b
Leandro Faustino <leandrofaustiinoo@hotmail.com>
2024-01-10 19:24:46 -0300

    atualizando

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     $('form button').click(function(){
-        $('form').slideDown();
+        $('ul li').slideDown();
     })
-    $
+    $('ul li').click(function(){
+        $('ul li').slideUp();
+    })
+    $('form').on('submit', function(e){
+        console.log("submit");
+        e.preventDefault();
+
+    })
 })
